534cbb3200329dd2cc3006ad5678661780cf7d1e
vinodiWebcode <vinod.kumar@iwebcode.dev>
2022-01-24 10:10:26 +0530

    bug fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,6 @@ const Header = ({}: HeaderProps) => {
   return (
     <div>
       <header>
-        <h3>hello</h3>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/page-2/">
